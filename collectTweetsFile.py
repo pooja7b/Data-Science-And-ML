326a1d63a605f7716e6c7391c1fcea74d3673d14
Pooja Bharadwaj <pooja7b@users.noreply.github.com>
2018-01-09 22:45:46 +0530

    Update collectTweetsFile.py

--- collectTweetsFile.py
+++ collectTweetsFile.py
@@ -1,9 +1,9 @@
 from twython import Twython
 
-APP_KEY = "HJPwbpmvynoqrKs5F13KC3HGE"
-APP_SECRET = "G5VTpecAmsfDp9vlmoXIDjjqW7T81MIjSF0yz9MdjqUSt6VnuS"
-OAUTH_TOKEN = "713407156700073984-Ub9FiuF02oBssBLHVhvaq4HLYUHBka6"
-OAUTH_TOKEN_SECRET = "epY5EQcpmYQ8JAyt4lVu0o80sxzuKbiwv8kx0nceDUhow" 
+APP_KEY = "put api key here"
+APP_SECRET = "put api secret here"
+OAUTH_TOKEN = "put access token here"
+OAUTH_TOKEN_SECRET = "put access token secret here" 
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET) 
 
